a185e19b75a3b15a81b9c08f4ea694c51aede77a
willeaton04 <willeatonaz@gmail.com>
2025-02-07 11:03:15 -0800

    Inital Solution

--- SudokuIO.java
+++ SudokuIO.java
@@ -7,15 +7,28 @@ public class SudokuIO {
     //     // TODO: impliment using scanner
     // }
 
-    public static void displayBoard(SudokuPuzzle puzzle) {
+    public static void displayBoard(SudokuPuzzle puzzle, int numChecks, int numBacktracks) {
         int[][] board = puzzle.getBoard();
 
+        System.out.println("---Solution---");
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print(String.format(" %d ", board[i][j]));
             }
             System.out.println();
         }
+        System.out.println(String.format("Checks: %d, Backtracks: %d", numChecks, numBacktracks));
+
+
+    }
+
+    public static int[] positionToIndex(int pos) throws IllegalArgumentException {
+        if (pos < 0 || pos > 80) {
+            throw new IllegalArgumentException("Position must be between 0 and 80");
+        }
+        int row = pos / 9;
+        int col = pos % 9;
+        return new int[]{row, col};
     }
 
 
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,27 +1,61 @@
-
 public class SudokuSolver {
+
+    private static int numBacktracks;
+    private static int numChecks;
+
+
     public SudokuSolver() {
     }
  
     public static void main(String[] var0) {
-       int[][] board = new int[][]{{1, 2, 3, 0, 0, 0, 0, 0, 0},
-                                  {4, 5, 6, 0, 0, 0, 0, 0, 0},
-                                  {7, 8, 8, 0, 0, 0, 0, 0, 0},
+   
+       int[][] board = new int[][]{{1, 0, 0, 0, 0, 3, 0, 0, 0},
+                                  {0, 3, 0, 0, 0, 0, 0, 0, 5},
+                                  {0, 0, 4, 0, 0, 0, 0, 0, 0},
+                                  {0, 0, 0, 2, 0, 0, 0, 0, 0}, 
                                   {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
                                   {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                  {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                  {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                  {0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                  {0, 0, 6, 0, 0, 0, 0, 9, 0}, 
+                                  {0, 0, 0, 0, 8, 0, 0, 0, 0}, 
                                   {0, 0, 0, 0, 0, 0, 0, 0, 0}};
        SudokuPuzzle puzzle = new SudokuPuzzle(board);
-       SudokuIO.displayBoard(puzzle);
-       System.out.println("Expected: false");
-       System.out.println(puzzle.isValid());
+       solve(puzzle, 0);
     }
  
-    public static void solveSudoku(SudokuPuzzle puzzle, int pos) {
-         if (pos == 80 && puzzle.isValid()) {
-               SudokuIO.displayBoard(puzzle);
-         }
+    private static void solve(SudokuPuzzle puzzle, int cellID) {
+        
+        int[][] board = puzzle.getBoard();
+        int row = cellID / 9;
+        int col = cellID % 9;
+        
+        // Base case - reached end of board
+        if (cellID >= 81) {
+            numChecks++;
+            if (puzzle.isValid()) {
+                SudokuIO.displayBoard(puzzle, numChecks, numBacktracks);
+                System.exit(0);
+            }
+            return;
+        }
+        
+        // Skip filled cells
+        if (board[row][col] != 0) {
+            solve(puzzle, cellID + 1);
+            return;
+        }
+        
+        // Try values 1-9
+        for (int i = 1; i <= 9; i++) {
+            board[row][col] = i;
+            numChecks++;
+            if (puzzle.isValid()) {
+                solve(puzzle, cellID + 1);
+            }
+        }
+        
+        // Backtrack
+        numBacktracks++;
+        board[row][col] = 0;
     }
- }
+}
+ 
